aa29da70af668db9d35dbac112c190472a277f1b
SuccessPear <lethanhcong3920@gmail.com>
2024-01-01 20:55:16 +0700

    Add ex3 LINQ

---
+++ CSharpExercise/LINQ/Ex2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class LinqExercise02
+{
+    public static void SolutionSQL()
+    {
+        var arr1 = new[] { 1, 3, 5, 7, 9 };
+
+        Console.Write("\nLINQ : Find the number and its square of an array which is more than 20 : ");
+        Console.Write("\n------------------------------------------------------------------------\n");
+
+        var query = from int number in arr1
+                    let sqrtNum = number * number
+                    where sqrtNum > 20
+                    select new { number, sqrtNum };
+
+        foreach (var a in query)
+            Console.WriteLine(a);
+
+        Console.ReadLine();
+    }
+
+    public static void SolutionMethod()
+    {
+        var arr1 = new[] { 1, 3, 5, 7, 9 };
+
+        Console.Write("\nLINQ : Find the number and its square of an array which is more than 20 : ");
+        Console.Write("\n------------------------------------------------------------------------\n");
+
+        var query = arr1.Where(x => x * x > 20).Select(x => new { number = x, sqrtNum = x * x });
+
+        foreach (var a in query)
+            Console.WriteLine(a);
+
+        Console.ReadLine();
+    }
+}
---
+++ CSharpExercise/LINQ/Ex3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Exercise;
+class LinqExercise03
+{
+    public static void SolutionSQL()
+    {
+        int[] nums = new int[] { 5, 1, 9, 2, 3, 7, 4, 5, 6, 8, 7, 6, 3, 4, 5, 2 };
+
+        Console.Write("The numbers in the array are : \n");
+        Console.Write("  5, 1, 9, 2, 3, 7, 4, 5, 6, 8, 7, 6, 3, 4, 5, 2 \n\n");
+
+
+        var query = from x in nums
+                    group x by x into y
+                    orderby y.Key
+                    select y;
+
+        foreach (var arrEle in query)
+        {
+            Console.WriteLine("Num: " + arrEle.Key + "\t" + "Frequency: " + arrEle.Count() + "\t" + "Product: " + arrEle.Sum());
+        }
+        Console.WriteLine();
+    }
+
+    public static void SolutionMethod()
+    {
+        int[] nums = new int[] { 5, 1, 9, 2, 3, 7, 4, 5, 6, 8, 7, 6, 3, 4, 5, 2 };
+
+        Console.Write("The numbers in the array are : \n");
+        Console.Write("  5, 1, 9, 2, 3, 7, 4, 5, 6, 8, 7, 6, 3, 4, 5, 2 \n\n");
+
+
+        var query = nums.GroupBy(x => x).OrderBy(x => x.Key).Select(x => x);
+
+        foreach (var arrEle in query)
+        {
+            Console.WriteLine("Num: " + arrEle.Key + "\t" + "Frequency: " + arrEle.Count() + "\t" + "Product: " + arrEle.Sum());
+        }
+        Console.WriteLine();
+    }
+}
