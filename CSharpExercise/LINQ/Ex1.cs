e2bd2f75ded9d2cfdaee044e1c331fe71b1ab6ed
SuccessPear <lethanhcong3920@gmail.com>
2024-01-01 18:20:34 +0700

    Add ex1

---
+++ CSharpExercise/LINQ/Ex1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Exercise;
+class LinqExercise01
+{
+    public static void SolutionSQL()
+    {
+        string str;
+
+        Console.Write("\nLINQ : Display the characters and frequency of character from giving string : ");
+        Console.Write("\n----------------------------------------------------------------------------\n");
+        str = "media platform - solution group";
+        Console.Write("\n");
+
+        var query = from x in str
+                    group x by x into y
+                    orderby y.Key
+                    select y;
+        Console.Write("The frequency of the characters are :\n");
+        foreach (var arrEle in query)
+        {
+            Console.WriteLine("Character " + arrEle.Key + ": " + arrEle.Count() + " times");
+        }
+    }
+
+    public static void SolutionMethod()
+    {
+        string str;
+
+        Console.Write("\nLINQ : Display the characters and frequency of character from giving string : ");
+        Console.Write("\n----------------------------------------------------------------------------\n");
+        str = "media platform - solution group";
+        Console.Write("\n");
+
+        var query = str.GroupBy(x => x)
+               .OrderBy(y => y.Key)
+               .Select(y => y);
+        Console.Write("The frequency of the characters are :\n");
+        foreach (var arrEle in query)
+        {
+            Console.WriteLine("Character " + arrEle.Key + ": " + arrEle.Count() + " times");
+        }
+    }
+}
